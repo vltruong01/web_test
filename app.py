1567aa1051b1e869bcefde524f3be1fcfceb41cf
vltruong01 <157948765+vltruong01@users.noreply.github.com>
2025-03-03 10:43:21 +0700

    f
    f

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -7,13 +8,14 @@ def index():
     result = ''
     if request.method == 'POST':
         user_input = request.form['user_input']
-        if user_input == 'Ngọc Ánh':
-            result = 'Ngọc Ánh Yêu Vương Lộc Trường'
-        if user_input == 'Vương Lộc Trường':
-            result = 'Vương Lộc Trường Yêu Ngọc Ánh'
-        if user_input == 'anhsime':
-            result = 'Ánh Dú Bự'
+        responses = {
+            'Ngọc Ánh': 'Ngọc Ánh Yêu Vương Lộc Trường',
+            'Vương Lộc Trường': 'Vương Lộc Trường Yêu Ngọc Ánh',
+            'anhsime': 'Ánh Dú Bự'
+        }
+        result = responses.get(user_input, 'Không tìm thấy kết quả!')
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
